6f5b5ae7ceffb06dc76f53d9b893595e207bf6d7
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-08-05 22:41:29 -0300

    novo exc

---
+++ ex/ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número'))
+r1 = n * 2
+r2 = n * 3
+r3 = n ** (1/2)
+print(f'O dobro de {n} é {n * 2}')
+print(f'O triplo de {n} é {n * 3}')
+print(f'A Raiz quadrada de {n} é {n ** (1/2):.2f}')
