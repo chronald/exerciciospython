fd8ded343320a5c8a6122e9f86647f61471255c0
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-08-05 22:46:41 -0300

    novo exercicio

---
+++ ex/ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite a primeira nota'))
+n2 = float(input('Digite a segunda nota'))
+r = (n1 + n2) / 2
+
+print(f'Com as Notas{n1} e {n2} a média do aluno será: {(n1 + n2) / 2}')
