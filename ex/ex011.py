64910d9183468dffd5b105828cb4189c4f03d69b
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-08-08 19:06:11 -0300

    novo exercicio

---
+++ ex/ex011.py
@@ -0,0 +1,5 @@
+n1 = float(input('Largura da parede:'))
+n2 = float(input('Altura da parede'))
+
+print(f'Sua parede tem a dimensão de {n1}x{n2} e sua área é de {n1*n2}m².')
+print(f'Para pintar essa parede, você precisará de {(n1*n2) / 2}l de tinta')
