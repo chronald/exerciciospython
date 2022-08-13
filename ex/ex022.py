82874a82d811e6fd832bfc9010989609e936b153
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-08-12 21:37:26 -0300

    novo exerc

---
+++ ex/ex022.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome completo:')).strip()
+nome1= nome.split()
+print('ANALISANDO SEU NOME...')
+print(f'Seu nome em MAIUSCULAS é {nome.upper()}')
+print(f'Seu nome em minusculas é {nome.lower()}')
+print(f'Seu nome tem ao todo {len(nome) - nome.count(" ")} letras')
+print(f'Seu primeiro nome é {nome1 [0]} e o mesmo contem {len(nome1 [0])} letras ')
